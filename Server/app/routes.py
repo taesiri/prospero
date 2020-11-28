30934896d74952979a1e7a2e5d49b12300160a1e
taesiri <mtaesiri@gmail.com>
2020-11-28 15:14:36 +0330

    Added Modal to show post body

--- Server/app/routes.py
+++ Server/app/routes.py
@@ -1,6 +1,7 @@
 from app import app
 from flask import render_template
 import json
+import random
 
 @app.route('/')
 @app.route('/index')
@@ -8,8 +9,14 @@ def index():
 
     # Loading data
     engadget_posts = {}
-    with open('app/data/engadet.json') as f:
+    with open('app/data/engadget.json') as f:
       engadget_posts = json.load(f)
+
+    # fake id for Modals - This is just a hacky way to show multiple modals with different data
+    # the proper way is through JS, but I chose this way at this stage.
+    random_ids = random.sample(range(1, 100), len(engadget_posts))
+    for i, post in enumerate(engadget_posts):
+      post['modalid'] = random_ids[i]
 
     all_tweets = list(open('app/data/twitter.json'))
     tweets = [json.loads(t) for t in all_tweets]
